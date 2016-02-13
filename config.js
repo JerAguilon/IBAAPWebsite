3b4110ce728d5add4d7b785eb0c445a56170bcfe
jeraguilon <jeraguilon@gmail.com>
2016-02-13 04:18:27 -0500

    -Changed database to jeraguilon -Added GitHub style heat calender to chart -Commended out old chart

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {};
 
-config.mongoUri = 'mongodb://jeraguilon:abc123@ds059135.mongolab.com:59135/researchtsrb';
+config.mongoUri = 'mongodb://jeraguilon:123123@ds059135.mongolab.com:59135/researchtsrb';
 config.cookieMaxAge = 3 * 60 * 60 * 1000; //ms
 module.exports = config;
