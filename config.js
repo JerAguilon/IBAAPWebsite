9f2a0e95599a59bac0b1eb169f78ac438e463a97
gchrestman <gcc0073@gmail.com>
2016-06-05 14:44:26 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {};
 
-config.mongoUri = 'mongodb://<dbuser>:<dbpassword>@ds013192.mlab.com:13192/ibaap';
+config.mongoUri = 'mongodb://<dbuser>:<dbpassword>@ds059135.mlab.com:59135/researchtsrb';
 config.cookieMaxAge = 3 * 60 * 60 * 1000; //ms
 module.exports = config;
